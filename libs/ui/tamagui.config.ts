b0f8970a457ad8d37cf7c38696fc2f30e1f275ed
Janko Zhang <jankergg@gmail.com>
2025-09-27 22:58:19 -0700

    add tamagui refactors

--- libs/ui/tamagui.config.ts
+++ libs/ui/tamagui.config.ts
@@ -2,29 +2,8 @@ import { config as baseConfig } from '@tamagui/config/v2';
 import { createTamagui } from 'tamagui';
 import { shorthands } from '@tamagui/shorthands';
 
-const spotifyDark = {
-  ...baseConfig.themes.dark,
-  background: '#050505',
-  backgroundFocus: '#121212',
-  backgroundHover: '#18181b',
-  color: '#f4f4f5',
-  colorHover: '#ffffff',
-  accent: '#1ed760',
-  accentFocus: '#19c95c',
-  accentHover: '#23e06b',
-};
-
-const spotifyLight = {
-  ...baseConfig.themes.light,
-  background: '#f9fafb',
-  backgroundHover: '#f3f4f6',
-  backgroundFocus: '#e5e7eb',
-  color: '#111827',
-  colorHover: '#111827',
-  accent: '#1db954',
-  accentHover: '#1ed760',
-  accentFocus: '#1aa34a',
-};
+import { spotifyDarkTheme } from './themes/spotify-dark.theme';
+import { spotifyLightTheme } from './themes/spotify-light.theme';
 
 const config = createTamagui({
   ...baseConfig,
@@ -32,8 +11,8 @@ const config = createTamagui({
   defaultTheme: 'spotifyDark',
   themes: {
     ...baseConfig.themes,
-    spotifyDark,
-    spotifyLight,
+    spotifyDark: spotifyDarkTheme,
+    spotifyLight: spotifyLightTheme,
   },
 });
 
